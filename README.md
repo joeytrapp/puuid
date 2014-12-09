0de3773da639ab947728167151b5655a4d797bdf
Joey Trapp <joey@loadsys.com>
2014-12-09 08:38:20 -0600

    Added new options for formatting output

--- README.md
+++ README.md
@@ -4,6 +4,10 @@ Print a new uuid in the console.
 
 ## Options
 
-Use `-n <number>` to print that many uuids spread across newlines.
-
-Use `-x <number>` to specify the UUID version to use. Currently only version 1 and 4 are supported.
+* `-n <number>`: Print that many uuids spread across newlines.
+* `-x <number>`: Specify the UUID version to use. Currently only version 1 and 4 are supported.
+* `-s '<string>'`: String to be used as the separator when joining the list of uuids.
+* `-f '<string>'`: Format string applied to each uuid (%s replaced with uuid).
+* `-t`: Trim whitespace off of final string before printing.
+* `-v`: Show version
+* `-h`: Show help
--- main.go
+++ main.go
@@ -12,9 +12,12 @@ import (
 var (
 	n       = flag.Int("n", 1, "Number of uuids to create")
 	x       = flag.Int("x", 4, "Which uuid version to use (1 or 4")
+	f       = flag.String("f", "%s", "Format string used when adding uuid to list")
+	s       = flag.String("s", "\n", "Separator used when joining multiple uuids together")
+	t       = flag.Bool("t", false, "Trim whitespace before printing")
 	v       = flag.Bool("v", false, "Show version")
 	h       = flag.Bool("h", false, "Show help")
-	version = "1.0.1"
+	version = "1.1.0"
 )
 
 func main() {
@@ -38,9 +41,14 @@ func main() {
 			fmt.Println("-x must be 1 or 4")
 			os.Exit(1)
 		}
-		l = append(l, u)
+		l = append(l, fmt.Sprintf(*f, u))
 	}
-	fmt.Printf("%s", strings.TrimSpace(strings.Join(l, "\n")))
+	str := strings.Join(l, *s)
+	str = str + "\n"
+	if *t {
+		str = strings.TrimSpace(str)
+	}
+	fmt.Print(str)
 }
 
 func NewUUID(v int) (string, error) {
@@ -60,9 +68,12 @@ func HelpText() string {
 
 Creates a UUID and prints it. Can create multiple UUID's with -n <num>.
 
-	-v Show version
-	-h Show help
-	-n Number of uuids to create [1]
-	-x UUID generation version: 1 or 4 [4]
+	-v         Show version
+	-h         Show help
+	-n <num>   Number of uuids to create [1]
+	-x <num>   UUID generation version: 1 or 4 [4]
+	-f "<str>" Format string used when adding uuid to list
+	-s "<str>" Separator used when joining multiple uuids together
+	-t         Trim whitespace before printing
 `
 }
